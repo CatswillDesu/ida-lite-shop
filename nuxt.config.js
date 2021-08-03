0a1a34e6477605ba7f98d6a0703cd1946c9b73fd
Nick <nickdomochevsky@gmail.com>
2021-08-03 16:47:35 +0300

    setup scss with configuration and base colors variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,11 +27,15 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/style-resources'
   ],
+  styleResources: {
+    scss: [
+      '~/assets/variables.scss'
+    ]
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
