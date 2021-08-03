e6660b07f97fabbbd4d239c22af830680e3adf1a
Nick <nickdomochevsky@gmail.com>
2021-08-03 21:35:52 +0300

    default layout (header and categories navigation)

---
+++ components/CartIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="cart-icon">
+    <CartVector />
+    <div class="batch">0</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cart-icon {
+  position: relative;
+  padding: 5px 10px;
+
+  .batch {
+    position: absolute;
+    top: 0px;
+    right: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 14px;
+    height: 14px;
+    border-radius: 50%;
+    background-color: $grey-light;
+    font-size: 9px;
+    color: $white;
+  }
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,54 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'ida-lite-shop',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap'
+      }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: {
+    dirs: [`~/components`, `~/components/vector`]
+  },
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: ['@nuxtjs/eslint-module'],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ['@nuxtjs/axios', '@nuxtjs/pwa', '@nuxtjs/style-resources'],
+  styleResources: {
+    scss: ['~/assets/variables.scss', '~/assets/mixins.scss']
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    baseUrl: 'https://frontend-test.idalite.com/api'
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  // eslint-disable-next-line prettier/prettier
+  build: {}
+}
